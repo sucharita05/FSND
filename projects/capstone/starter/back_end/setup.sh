c5672e0c6e566413c9602d9c03b91f59008cbd26
sucharita05 <68145021+sucharita05@users.noreply.github.com>
2021-04-22 20:04:17 +0530

    code

--- projects/capstone/starter/back_end/setup.sh
+++ projects/capstone/starter/back_end/setup.sh
@@ -1,1 +1,5 @@
-
+export AUTH0_DOMAIN='starinmaking.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='http://localhost:5000'
+export SECRET='jC905OeKFFgTOCas_nxrjMH5rkGcFo9P1IDBlrnKAsDsdQH2PiJE6fuYSMkXJ2Or'
+export DATABASE_URL='postgres://vemzpndanyxaft:6bca144dad71cdbfa0c888ece7d91b5272f40493efc7277d4aee339782844f0a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d20ksd0amd8ivm'
